c93dc347dbca903ac99a10fa1dfaf073848487ae
rasakmarsawa <rasakmarsawa@gmail.com>
2025-10-28 10:36:58 +0700

    test bad CI

--- tests/Feature/AttendanceCheckTest.php
+++ tests/Feature/AttendanceCheckTest.php
@@ -47,6 +47,8 @@ class AttendanceCheckTest extends TestCase
             'user_id' => $user->id,
             'date' => now()->toDateString()
         ]);
+
+        $this->assertTrue(false);
     }
 
     #[\PHPUnit\Framework\Attributes\Test]
