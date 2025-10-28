a6e8489be6c321c522881e7cb9f58573896ef42e
rasakmarsawa <rasakmarsawa@gmail.com>
2025-10-28 09:45:45 +0700

    test with good test

--- tests/Feature/AttendanceCheckTest.php
+++ tests/Feature/AttendanceCheckTest.php
@@ -47,9 +47,6 @@ class AttendanceCheckTest extends TestCase
             'user_id' => $user->id,
             'date' => now()->toDateString()
         ]);
-
-        $this->assertTrue(false);
-        $this->assertTrue(false);
     }
 
     #[\PHPUnit\Framework\Attributes\Test]
