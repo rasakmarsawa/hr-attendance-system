84b19c05e42ba90aa1df3ee4d6a7b0979e19d528
rasakmarsawa <rasakmarsawa@gmail.com>
2025-10-29 10:17:51 +0700

    fix schedule import

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,6 +3,8 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Foundation\Scheduling\Schedule;
+
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
