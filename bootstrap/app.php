c11edeee8511f7fb86ef650231a010fc4f99d7bb
rasakmarsawa <rasakmarsawa@gmail.com>
2025-10-29 08:59:13 +0700

    update bootstrap/app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,6 +3,8 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Support\Facades\URL;
+use Illuminate\Http\Request;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -17,4 +19,22 @@ return Application::configure(basePath: dirname(__DIR__))
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
-    })->create();
+    })
+    ->create()
+    // -------------------------------
+    // TRUST PROXIES (for Render load balancer)
+    // -------------------------------
+    ->tap(function (Application $app) {
+        $request = $app->make(Request::class);
+        $request->setTrustedProxies(
+            ['0.0.0.0/0'], // trust all proxies
+            Request::HEADER_X_FORWARDED_ALL
+        );
+
+        // -------------------------------
+        // FORCE HTTPS IN PRODUCTION
+        // -------------------------------
+        if (env('APP_ENV') === 'production') {
+            URL::forceScheme('https');
+        }
+    });
