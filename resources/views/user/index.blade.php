2f2d5d7fc2c9a0abf34a8476baa9fe8dcbb46bc5
rasakmarsawa <rasakmarsawa@gmail.com>
2025-10-29 10:37:00 +0700

    add session error on user.index

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -24,6 +24,25 @@
                 </div>
             </div>
         @endif
+        @if(session('error'))
+            <div class="mb-4">
+                <div class="rounded-md bg-red-50 p-4">
+                    <div class="flex">
+                        <div class="flex-shrink-0">
+                            <svg class="h-5 w-5 text-red-400" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                    d="M12 8v4m0 4h.01M21 12A9 9 0 113 12a9 9 0 0118 0z" />
+                            </svg>
+                        </div>
+                        <div class="ml-3">
+                            <p class="text-sm font-medium text-red-800">
+                                {{ session('error') }}
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        @endif
         <div class="mb-4 flex items-center justify-between">
             <div>
                 <h3 class="text-lg font-medium text-gray-900">All users</h3>
